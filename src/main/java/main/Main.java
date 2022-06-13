2755cbb505344af7c13c506783ef66b1a3863b05
artym <artymk77@gmail.com>
2022-06-13 09:35:56 +0300

    add new servlets

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -39,6 +39,7 @@ public class Main {
         server.setHandler(handlers);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
---
+++ src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,7 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class SignInServlet extends HttpServlet {
+
+}
